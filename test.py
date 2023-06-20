a16e5deab2a76bf3c6d2d65878ada57f5066bd8d
arojaspe <arojaspe@unal.edu.co>
2023-06-20 12:01:26 -0500

    Arreglo funciones suma y multiplicación para campos GF(2^3

--- test.py
+++ test.py
@@ -4,28 +4,30 @@ import galois
 GF = galois.GF(2**3)
 
 #Con esto accedemos a ls propiedades del campo finito. Lo que nos interesa es el polinomio irreducible
-
 print("------Propiedades------"+"\n"+GF.properties)
 
 #esto es cómo se representan todos los polinomios posibles con el campo finito que se definió
 print("------Tabla representaciones------"+"\n"+GF.repr_table())
 
 #Aquí se inicializan los polinomios con aquello de aritmética modular
-p1 = GF([1, 1, 1])  # Representa el polinomio x^2 + x + 1       → 1 1 1
-p2 = GF([1, 0, 1])  # Representa el polinomio x^2 + 1           → 1 0 1
+p1 = galois.Poly([1, 1, 1], field= GF)  # Representa el polinomio x^2 + x + 1       → 1 1 1
+p2 = galois.Poly([1, 0, 1], field= GF)  # Representa el polinomio x^2 + 1           → 1 0 1
 
 #Operaciones que se pueden hacer con los polinomios sobre el campo finito
 suma = p1+p2
+#Con esto accedemos a los coeficientes del polinomio irreducible
 irreducible_poly = GF.irreducible_poly
 print("Coeficientes polinomio irreducible: ",GF.irreducible_poly.coefficients())
-multiplication = (p1*p2)#%GF.irreducible_poly.coefficients()
-subs = p1-p2
 
-print("Polinomio 1: ",galois.Poly(p1))
-print("Polinomio 2: ",(galois.Poly(p2)))
+#Aquí tocó poner el polinomio irreducible como vector porque no supe extraerlo del irreducible_poly ↓
+a = galois.Poly([1, 0, 1, 1], field=GF)
 
-polySuma=galois.Poly(suma)
-polyMult = galois.Poly(multiplication)
+print("Polinomio 1: ",p1)
+print("Polinomio 2: ",p2)
+
+polySuma=p1+p2
+polyMult = p1*p2%a
 print("Suma: ",polySuma)
 print("----")
-print("Multiplicación",polyMult)
+print("Multiplicación pre-módulo",p1*p2)
+print("Multiplicación post-módulo",polyMult)
